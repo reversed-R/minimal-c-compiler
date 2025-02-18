f990842b489cde0699a186348c72f1a97fc22316
reversed_R <qiu1287shan@gmail.com>
2025-02-19 01:50:01 +0900

    feat: an integer compiler

---
+++ an_integer_compiler/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+assert() {
+  expected="$1"
+  input="$2"
+  
+  ./mycc "$input" > tmp.s
+  gcc tmp.s -o tmp.out
+  ./tmp.out
+  actual="$?"
+  rm tmp.s
+  rm tmp.out
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input" -> "$actual"
+  else
+    echo "$input" -> "$expected" expected, but got "$actual"
+    exit 1
+  fi
+}
+
+assert 0 0 
+assert 42 42
+
+echo OK
