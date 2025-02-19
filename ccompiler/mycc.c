d76e891a659ad8d06c0e0cb76022be0550540110
reversed_R <qiu1287shan@gmail.com>
2025-02-19 14:14:34 +0900

    feat: LL(1)

--- ccompiler/mycc.c
+++ ccompiler/mycc.c
@@ -20,6 +20,89 @@ struct Token {
   char *str;
 };
 
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_INT,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs; // left hand side
+  Node *rhs; // right hand side
+  int val;
+};
+
+/* foward declarations *********/
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_int(int val);
+Node *expr();
+void error_at(char *loc, char *fmt, ...);
+bool consume(char op);
+void expect(char op);
+int expect_int();
+Token *tokenize(char *p);
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Node *mul();
+Node *primary();
+/******** foward declarations */
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_int(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_INT;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_int(expect_int());
+}
+
 Token *token;
 char *user_input;
 
