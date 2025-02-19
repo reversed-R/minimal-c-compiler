cfcfd9a684850c8a259ef6866b1a5994d9e23fae
reversed_R <qiu1287shan@gmail.com>
2025-02-19 13:26:41 +0900

    feat: tokenizer

--- ccompiler/mycc.c
+++ ccompiler/mycc.c
@@ -1,5 +1,95 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
+
+Token *token;
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+
+  token = token->next;
+  return true;
+}
+
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("'%c' not expected", op);
+  token = token->next;
+}
+
+int expect_int() {
+  if (token->kind != TK_NUM)
+    error("integer value expected");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() { return token->kind == TK_EOF; }
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("failed to tokenize");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,10 +97,27 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  token = tokenize(argv[1]);
+
+  // foward part of assembly
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+
+  // the head of expr must be integer
+  printf("  mov rax, %d\n", expect_int());
+
+  // process sequence of `+ "integer"` | `- "integer"`
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_int());
+      continue;
+    }
+
+    expect('-');
+    printf("  sub rax, %d\n", expect_int());
+  }
+
   printf("  ret\n");
   return 0;
 }
